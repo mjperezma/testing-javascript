984fca1ab2fe5f4322c5b755f67595293b4649b6
mj0se <mariajose1907@gmail.com>
2020-03-25 22:03:00 +0100

    testing keydwon audio

---
+++ javascript-drum/js/main.js
@@ -0,0 +1,24 @@
+'use strict';
+
+
+let key = document.querySelector('.key');
+
+
+function pushKey(ev) {
+    const playAudio = document.querySelector(`audio[data-key="${ev.keyCode}"]`);
+    const playKey = document.querySelector(`.key[data-key="${ev.keyCode}"]`);
+    if (!playAudio) return;
+    playAudio.currentTime = 0;
+    playAudio.play();
+    playKey.classList.add('playing');
+
+}
+
+window.addEventListener('keydown', pushKey);
+
+function removeTransition(e) {
+    if (e.propertyName !== 'transform') return;
+    this.classList.remove('playing');
+}
+const keys = document.querySelectorAll('.key');
+keys.forEach(key => key.addEventListener('trasitionend', removeTransition));
